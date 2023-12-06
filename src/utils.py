ad6975f46db73a904aff205b74c00d356760962b
Vanya <balanaaarrrr@gmail.com>
2023-12-06 18:51:39 +0300

    get last page on Playwright

--- src/utils.py
+++ src/utils.py
@@ -24,4 +24,19 @@ async def get_last_page(session: ClientSession, url: str):
         last_page = [i.find('a').text for i in soup][-2]
         return int(last_page)
 
+async def get_last_page_v2(url:str):
+    logger.info('Getting last page....')
+    async with async_playwright() as playwright:
+        browser = await playwright.firefox.launch()
+        context = await browser.new_context(extra_http_headers=HEADERS)
+        page = await context.new_page()
+        result = await page.goto(url)
+        logger.info(f'{url} | {result.status}')
+        assert result.status == 200
 
+        list_content = await page.query_selector_all('li.MPProductPagination-PageItem')
+        last_page = [await k.inner_text() for k in [await i.query_selector('a') for i in list_content]][-2]
+        return int(last_page)
+
+
+# async def get_product_links
